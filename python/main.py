1cb77e750a3eca893c3b45401567db768474ee06
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2017-05-06 16:17:09 -0400

    adding

--- python/main.py
+++ python/main.py
@@ -17,7 +17,8 @@ def main():
     #fourcc = cv2.VideoWriter_fourcc(*'XVID')
     #out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
 
-    out = cv2.VideoWriter('../data/output.avi', -1, 20.0, (640,480))
+    #out = cv2.VideoWriter('../data/output.avi', -1, 20.0, (640,480))
+    out = cv2.VideoWriter('..\data\output.avi', -1, 20.0, (640,480))
 
     # define mongodb database
 
@@ -57,12 +58,11 @@ def main():
 	    out.write(frame)
 
 	    cv2.imshow('frame',frame)
-	    if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+            if cv2.waitKey(1) == 0x1b:
+		print 'ESC pressed. Exiting ...'
+                break
 	else:
 	    break
-
-
 
         ts = time.time()
 	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
